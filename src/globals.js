e8db6507b78bb1e6cd38a6e26c665a2f0a32b51c
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-02-03 09:03:29 +0100

    feat: create game engine & its necessary variables

---
+++ src/globals.js
@@ -0,0 +1,19 @@
+export default {
+    // |||||||||||| CANVAS & CONTEXT
+    canvas: {},
+    ctx: {},
+
+    // |||||||||||| PREVIOUS CYCLE TIME (MILLISECONDS)
+    previousCycleMilliseconds: -1,
+
+    // |||||||||||| ACTUAL GAME CYCLE TIME (SECONDS)
+    deltaTime: 0,
+
+    // |||||||||||| GOAL CYCLE TIME (SECONDS, CONSTANT)
+    frameTimeObj: 0,
+
+    cycleRealTime: 0,
+
+    // |||||||||||| OBJECT THAT HOLDS THE STATES OF THE KEYBOARD KEYS
+    action: {},
+};
