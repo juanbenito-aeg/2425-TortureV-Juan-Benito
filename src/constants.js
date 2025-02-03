8fb404250cea7fffd242ed2a546bf703f85c94d3
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-02-03 09:33:46 +0100

    feat: create the game's elements identifiers

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+// |||||||||||| FPS (FRAMES PER SECOND)
+const FPS = 30;
+
+// |||||||||||| MAP BLOCK IDENTIFIER
+const BlockID = {
+    CORRIDOR: 0,
+    WALL: 1,
+};
+
+// |||||||||||| MAP BLOCK'S UNICODE
+const BlockUnicode = {
+    WALL: "\u2B1C",    
+};
+
+// |||||||||||| ELEMENT IDENTIFIER
+const ElementID = {
+    MONEY: 0,
+    HEART: 1,
+};
+
+// |||||||||||| ELEMENT'S UNICODE
+const ElementUnicode = {
+    MONEY: "\u{1F4B5}",
+    HEART: "\u{1F49A}",
+};
+
+// |||||||||||| CHARACTER IDENTIFIER
+const CharacterID = {
+    PLAYER: 0,
+    SPIDER: 1,
+};
+
+// |||||||||||| CHARACTER'S UNICODE
+const CharacterUnicode = {
+    PLAYER: "\u{1F474}",
+    SPIDER: "\u{1F577}",
+};
+
+export { FPS, BlockID, CharacterID };
