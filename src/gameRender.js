46a4d0fbf4bb896b82d7824c0dd890e3f8172274
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2025-02-03 11:12:00 +0100

    feat: draw the player on screen

--- src/gameRender.js
+++ src/gameRender.js
@@ -1,19 +1,17 @@
 import globals from "./globals.js";
-import { BlockID, BlockUnicode } from "./constants.js";
+import { BlockID, BlockUnicode, CharacterID, CharacterUnicode } from "./constants.js";
 
 export default function render() {
     // |||||||||||| CLEAR SCREEN
     globals.ctx.clearRect(0, 0, globals.canvas.width, globals.canvas.height);
     
     renderMap();
+    renderCharacters();
 }
 
 function renderMap() {
     globals.ctx.fillStyle = "black";
     globals.ctx.fillRect(0, 0, globals.canvas.width, globals.canvas.height);
-
-    const initialBlockXCoordinate = 40;
-    const initialBlockYCoordinate = 30;
 
     const blockSize = 16;
 
@@ -21,12 +19,37 @@ function renderMap() {
 
     for (let i = 0; i < globals.map.length; i++) {
         for (let j = 0; j < globals.map[0].length; j++) {
-            const blockXCoordinate = initialBlockXCoordinate + (j * blockSize);
-            const blockYCoordinate = initialBlockYCoordinate + ((i + 1) * blockSize);
+            const blockXCoordinate = globals.mapInitialXCoordinate + (j * blockSize);
+            const blockYCoordinate = globals.mapInitialYCoordinate + ((i + 1) * blockSize);
 
             if (globals.map[i][j] === BlockID.WALL) {
                 globals.ctx.fillText(BlockUnicode.WALL, blockXCoordinate, blockYCoordinate);
             }
         }
+    }
+}
+
+function renderCharacters() {
+    const blockSize = 16;
+
+    globals.ctx.font = "14px emulogic";
+
+    for (let i = 0; i < globals.characters.length; i++) {
+        const currentCharacterXCoordinate = globals.mapInitialXCoordinate + (globals.characters[i].mapColIndex * blockSize);
+        const currentCharacterYCoordinate = globals.mapInitialYCoordinate + ((globals.characters[i].mapRowIndex + 1) * blockSize);
+        
+        let currentCharacterUnicode;
+        
+        switch (globals.characters[i].id) {
+            case CharacterID.PLAYER:
+                currentCharacterUnicode = CharacterUnicode.PLAYER;
+                break;
+            
+            case CharacterID.SPIDER:
+                currentCharacterUnicode = CharacterUnicode.SPIDER;
+                break;
+        }
+
+        globals.ctx.fillText(currentCharacterUnicode, currentCharacterXCoordinate, currentCharacterYCoordinate);
     }
 }
